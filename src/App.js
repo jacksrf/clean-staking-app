af17e327f51f5bfe1f5c34a334a5e8b177c9d6a4
Trey Jackson <treyjackson@Trey-Jackson-MacBook-Pro.local>
2022-04-15 21:52:19 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { useWeb3React } from '@web3-react/core'
+
+const CoinbaseWallet = new WalletLinkConnector({
+ url: `https://mainnet.infura.io/v3/fe4825c522934c39abe67676feb87167`,
+ appName: "Web3-react Demo",
+ supportedChainIds: [1, 3, 4, 5, 42],
+});
+
+const WalletConnect = new WalletConnectConnector({
+ rpcUrl: `https://mainnet.infura.io/v3/fe4825c522934c39abe67676feb87167`,
+ bridge: "https://bridge.walletconnect.org",
+ qrcode: true,
+});
+
+const Injected = new InjectedConnector({
+ supportedChainIds: [1, 3, 4, 5, 42]
+});
+
 function App() {
+  const { activate, deactivate } = useWeb3React();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => { activate(CoinbaseWallet) }}>Coinbase Wallet</button>
+      <button onClick={() => { activate(WalletConnect) }}>Wallet Connect</button>
+      <button onClick={() => { activate(Injected) }}>Metamask</button>
+
+      <button onClick={deactivate}>Disconnect</button>
     </div>
   );
 }
